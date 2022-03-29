e548c5686f4a72ef441ab1a05a5896eb2cd846df
Harrison Bludworth <harrisonbludworth@harrisons-mbp-2.lan>
2022-03-29 13:20:51 -0600

    updated frontend page

--- packages/frontend/src/views/PlaybackPage.vue
+++ packages/frontend/src/views/PlaybackPage.vue
@@ -1,6 +1,47 @@
 <template>
   <div>
-    <playback :audioUrl="`http://localhost:8081/api/v1/audio/${uuid}`" />
+    <v-container class="ma-0 pa-10" fluid>
+      <v-row class="justify-center">
+        <v-img
+          src="http://localhost:8081/api/v1/image/logo"
+          max-width="300px"
+          class="mb-12 mt-8"
+        />
+      </v-row>
+      <v-row class="justify-center mt-10">
+        <h3>Hey! You've received an audio message through MicDrop!</h3>
+      </v-row>
+      <v-row class="justify-center text--disabled">
+        <span> To listen, simply hit the play button below.</span>
+      </v-row>
+      <v-row class="justify-center my-12">
+        <playback :audioUrl="`http://localhost:8081/api/v1/audio/${uuid}`" />
+      </v-row>
+      <v-row class="justify-center">
+        <h3>Want to send your own audio messages?</h3>
+      </v-row>
+      <v-row class="justify-center text--disabled my-4">
+        <span>With the <a>MicDrop Chrome Extension</a>, you can:</span>
+      </v-row>
+      <v-row class="justify-center text--disabled">
+        <span>- Reply to this message with your own recording</span>
+      </v-row>
+      <v-row class="justify-center text--disabled">
+        <span>- Send your own audio emails to anyone</span>
+      </v-row>
+      <v-row class="justify-center text--disabled">
+        <span>- Listen to MicDrop messages directly within Gmail</span>
+      </v-row>
+      <v-row class="justify-center text--disabled">
+        <span>- And so much more!</span>
+      </v-row>
+      <!-- <v-row class="justify-center text--disabled">
+        <span
+          >Add our Chrome Extension <a>here</a> to reply with a MicDrop audio
+          message, send your own audio emails, and listen within Gmail.</span
+        >
+      </v-row> -->
+    </v-container>
   </div>
 </template>
 
@@ -24,3 +65,11 @@ export default defineComponent({
   },
 });
 </script>
+
+<style scoped>
+.full_screen {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+</style>
