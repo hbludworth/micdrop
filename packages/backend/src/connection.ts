9f80ffbc2bbe8eb96dc6dcd94229c5f19368246a
Harrison Bludworth <harrisonbludworth@Harrisons-MacBook-Pro-2.local>
2022-05-12 23:01:10 -0600

    created knex connections and users migration

---
+++ packages/backend/src/connection.ts
@@ -0,0 +1,18 @@
+import knex, { Knex } from 'knex';
+
+export const knexConfig: Knex.Config = {
+  client: 'mysql',
+  connection:
+    process.env.NODE_ENV === 'production'
+      ? process.env.MICDROP_DB_URL
+      : {
+          host: 'localhost',
+          user: 'root',
+          port: 3306,
+          password: 'example',
+          database: 'micdrop',
+        },
+  pool: { min: 0, max: 10 },
+};
+
+export default knex(knexConfig);
