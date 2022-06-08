8c0219ba21d8aa062aaeb45db1c51e3aec0481c0
Harrison Bludworth <harrisonbludworth@gmail.com>
2022-06-07 18:01:31 -0600

    extension popup dynamically adjusts iframe src based on env

--- packages/extension/src/App.vue
+++ packages/extension/src/App.vue
@@ -3,18 +3,25 @@
     <iframe
       width="170px"
       height="240px"
-      src="http://localhost:8080/extension/popup"
+      :src="iframeSource"
       style="top: 0; left: 0; border: 0"
     />
   </v-app>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "@vue/composition-api";
+import { defineComponent, ref } from "@vue/composition-api";
 
 export default defineComponent({
   setup() {
-    return {};
+    const iframeSource = ref(
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:8080/extension/popup"
+        : "https://sendmicdrop.com/extension/popup"
+    );
+    return {
+      iframeSource,
+    };
   },
 });
 </script>
