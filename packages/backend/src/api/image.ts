4fa7e513ffd401946299e7afed39ea4a33a49527
hbludworth <63749412+hbludworth@users.noreply.github.com>
2022-07-22 20:06:25 -0600

    added placeholder image route (#81)

--- packages/backend/src/api/image.ts
+++ packages/backend/src/api/image.ts
@@ -39,4 +39,27 @@ router.route('/image/:key').get(async (req, res, next) => {
   }
 });
 
+router.route('/placeholder_image/:key').get(async (req, res, next) => {
+  try {
+    const s3 = new AWS.S3();
+
+    const { key } = req.params;
+
+    const params = {
+      Bucket: 'micdrop-placeholder-images',
+      Key: key,
+    };
+
+    const image = (await s3.getObject(params).promise()).Body;
+
+    if (image) {
+      res.send(image).end();
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    next(new HttpInternalError(err as string));
+  }
+});
+
 export default router;
