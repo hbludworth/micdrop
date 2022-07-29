63bb5cdfbb1b34b0285cc22bc650e34ef906c9a5
Harrison Bludworth <harrisonbludworth@gmail.com>
2022-07-29 02:09:13 -0600

    fixed placeholder text bug

--- zip.sh
+++ zip.sh
@@ -1,6 +1,6 @@
 yarn build
 cd packages/backend
-zip -q -r micdrop-build.zip build dist package.json yarn.lock .platform
+zip -q -r micdrop-build.zip build dist package.json yarn.lock .platform fonts
 mv micdrop-build.zip ../..
 cd ../..
 mkdir micdrop-build
@@ -8,7 +8,7 @@ unzip micdrop-build.zip -d ./micdrop-build
 cd micdrop-build
 yarn install
 npm rebuild --platform=linux --arch=arm64 sharp
-zip -q -r micdrop-build.zip build dist package.json yarn.lock .platform node_modules
+zip -q -r micdrop-build.zip build dist package.json yarn.lock .platform node_modules fonts
 mv micdrop-build.zip ../
 cd ..
 rm -rf micdrop-build
