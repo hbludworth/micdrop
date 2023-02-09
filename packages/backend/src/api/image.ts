8e3e6e339bb3316afee9d5112a2c3452b710adac
Harrison Bludworth <harrisonbludworth@gmail.com>
2023-02-08 17:02:41 -0700

    added image file retrieval support to server

--- packages/backend/src/api/image.ts
+++ packages/backend/src/api/image.ts
@@ -31,6 +31,29 @@ router.route('/image/:key').get(async (req, res, next) => {
   }
 });
 
+router.route('/image_file/:key').get(async (req, res, next) => {
+  try {
+    const s3 = new AWS.S3();
+
+    const { key } = req.params;
+
+    const params = {
+      Bucket: 'micdrop-images',
+      Key: key,
+    };
+
+    const image = (await s3.getObject(params).promise()).Body;
+
+    if (image) {
+      res.send(image).end();
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    next(new HttpInternalError(err as string));
+  }
+});
+
 router.route('/placeholder_image/:key').get(async (req, res, next) => {
   try {
     const s3 = new AWS.S3();
